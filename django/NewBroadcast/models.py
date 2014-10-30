6f9426836771cfdab9a15ca56680fa6b746e945a
yuantailing <yuantailing@126.com>
2014-10-30 14:24:00 +0800

    NEWB-22 models代码风格及User、ProgramGroup单元测试

--- django/NewBroadcast/models.py
+++ django/NewBroadcast/models.py
@@ -4,63 +4,127 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    email = models.EmailField(blank = False, default = None, unique = True)
-    password = models.TextField(blank = False, default = None)
-    nickname = models.TextField(blank = False, default = None, unique = True)
-    power = models.CharField(blank = False, default = 'user', max_length = 32, 
-            choices = (('user','user'),('admin','admin'),
-                       ('administrator','administrator')))
-    birthday = models.DateTimeField(null = True, blank = True, default = None)
-    phone_number = models.CharField(null = True, blank = True, default = None, max_length = 32)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    email = models.EmailField(blank=False, default=None, unique=True)
+    password = models.TextField(blank=False, default=None)
+    nickname = models.TextField(blank=False, default=None, unique=True)
+    power = models.CharField(blank=False, default='user', max_length=32,
+                             choices=(('user', 'user'), ('admin', 'admin'),
+                                      ('administrator', 'administrator')))
+    birthday = models.DateTimeField(null=True, blank=True, default=None)
+    phone_number = models.CharField(null=True, blank=True, default=None,
+                                    max_length=32)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.email:
+            self.email = None
+        if not self.password:
+            self.password = None
+        if not self.nickname:
+            self.nickname = None
+        if not self.power:
+            self.power = None
+        if not self.phone_number:
+            self.phone_number = None
+        super(User, self).save()
+
 
 class ProgramGroup(models.Model):
-    title = models.TextField(blank = False, default = None)
-    description = models.TextField(null = True, blank = True, default = None)
-    weight = models.IntegerField(blank = False, default = 0)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    title = models.TextField(blank=False, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    weight = models.IntegerField(blank=False, default=0)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.title:
+            self.title = None
+        if not self.description:
+            self.description = None
+        super(ProgramGroup, self).save()
+
 
 class ProgramSeries(models.Model):
-    group_id = models.IntegerField(blank = False, default = None, db_index = True)
-    title = models.TextField(blank = False, default = None)
-    description = models.TextField(null = True, blank = True, default = None)
-    weight = models.IntegerField(blank = False, default = 0)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    group_id = models.IntegerField(blank=False, default=None, db_index=True)
+    title = models.TextField(blank=False, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    weight = models.IntegerField(blank=False, default=0)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.title:
+            self.title = None
+        if not self.description:
+            self.description = None
+        super(ProgramSeries, self).save()
+
 
 class Program(models.Model):
-    series_id = models.IntegerField(blank = False, default = None, db_index = True)
-    title = models.TextField(blank = False, default = None)
-    description = models.TextField(null = True, blank = True, default = None)
-    weight = models.IntegerField(blank = False, default = 0)
-    page_format = models.TextField(null = True, blank = True, default = None)
-    recorder = models.TextField(null = True, blank = True, default = None)
-    source = models.TextField(null = True, blank = True, default = None)
-    workers = models.TextField(null = True, blank = True, default = None)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    series_id = models.IntegerField(blank=False, default=None, db_index=True)
+    title = models.TextField(blank=False, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    weight = models.IntegerField(blank=False, default=0)
+    page_format = models.TextField(null=True, blank=True, default=None)
+    recorder = models.TextField(null=True, blan=True, default=None)
+    source = models.TextField(null=True, blank=True, default=None)
+    workers = models.TextField(null=True, blank=True, default=None)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.title:
+            self.title = None
+        if not self.description:
+            self.description = None
+        if not self.page_format:
+            self.page_format = None
+        if not self.recorder:
+            self.recorder = None
+        if not self.source:
+            self.source = None
+        if not self.workers:
+            self.workers = None
+        super(Program, self).save()
+
 
 class Source(models.Model):
-    program_id = models.IntegerField(blank = False, default = None, db_index = True)
-    source_type = models.TextField(blank = False, default = None)
-    title = models.TextField(null = True, blank = True, default = None)
-    description = models.TextField(null = True, blank = True, default = None)
-    link = models.TextField(null = True, blank = True, default = None)
-    doc = models.FileField(null = True, blank = True, default = None, upload_to = 'source/')
-    weight = models.IntegerField(blank = False, default = 0)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    program_id = models.IntegerField(blank=False, default=None, db_index=True)
+    source_type = models.TextField(blank=False, default=None)
+    title = models.TextField(null=True, blank=True, default=None)
+    description = models.TextField(null=True, blank=True, default=None)
+    link = models.TextField(null=True, blank=True, default=None)
+    doc = models.FileField(null=True, blank=True, default=None,
+                           upload_to='source/')
+    weight = models.IntegerField(blank=False, default=0)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.title:
+            self.title = None
+        if not self.description:
+            self.description = None
+        if not self.source_type:
+            self.source_type = None
+        if not self.link:
+            self.link = None
+        super(Source, self).save()
+
 
 class Comment(models.Model):
-    uid = models.IntegerField(blank = False, default = None, db_index = True)
-    program_id = models.IntegerField(blank = False, default = None, db_index = True)
-    content = models.TextField(blank = False, default = None)
-    update_time = models.DateTimeField(auto_now_add = True, auto_now = True)
-    create_time = models.DateTimeField(auto_now_add = True)
+    uid = models.IntegerField(blank=False, default=None, db_index=True)
+    program_id = models.IntegerField(blank=False, default=None, db_index=True)
+    content = models.TextField(blank=False, default=None)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def save(self):
+        if not self.content:
+            self.content = None
+        super(Comment, self).save()
 
 from django.contrib import admin
 admin.site.register((User, ProgramGroup, ProgramSeries, Program,
                      Source, Comment))
-
