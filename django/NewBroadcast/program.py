21112c7671e7a1a39e39d01302b03c63f962e415
yuantailing <yuantailing@126.com>
2014-11-06 11:45:29 +0800

    NEWB-40 从控制器传入参数供模板渲染

--- django/NewBroadcast/program.py
+++ django/NewBroadcast/program.py
@@ -10,7 +10,20 @@ import json
 from models import *
 
 def show_program(req, arg):
-    return render_to_response("program/show.html", {});
+    pgid = int(arg)
+    # pg = Program.objects.get(id=pgid)
+    title = u"海燕 - 高尔基"
+    description = u"此文按海面景象的发展变化分成三部分，描绘了海燕面临狂风暴雨和波涛翻腾的大海时的壮丽图画。在这篇文章中，作者通过对海燕在暴风雨来临之际勇敢欢乐的形象的描写，深刻反映了1905年俄国革命前夕急剧发展的革命形势，热情歌颂了俄国无产阶级革命先驱坚强无畏的战斗精神"
+    medialink = u"/upload/source/sample.mp3"
+    table = [(u"播音人员", u"这里填录音/播音人员名单"),
+             (u"稿件来源", u"这里填稿件来源"),
+             (u"机务人员", u"这里填机务人员名单")]
+    piclink = "/static/images/1.jpg"
+    return render_to_response("program/show.html",
+                    {'pgid':pgid, 'title':title,
+                     'strong':description[0:1], 'description':description[1:],
+                     'medialink':medialink, 'piclink':piclink,
+                     'table':table});
 
 def play_program(req, arg):
     return render_to_response("program/player.html", {});
