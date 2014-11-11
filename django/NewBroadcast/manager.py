eb2182fdaea8bae53f6be67ed9ff793e6bc7ce5a
knight <huanglijian1005@163.com>
2014-11-11 13:30:17 +0800

    NEWB-71 manageer

---
+++ django/NewBroadcast/manager.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+from django.http import HttpResponse
+from django.shortcuts import *
+from django import template
+from django.template.loader import get_template
+from django.db import models
+from django.core import serializers
+
+from models import *
+def show_space(req):
+    return render_to_response("manager/space.html", {});
+
+def show_mgrres(req):
+    return render_to_response("manager/mgrres.html", {});
+
+def show_mgruser(req):
+    return render_to_response("manager/mgruser.html", {});
