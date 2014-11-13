dca59dec8ec5e4ab9536e8f1f6e4ba650d811e63
= <loyaveforever@gmail.com>
2014-11-13 22:26:54 +0800

    change front page style

--- django/static/js/waterflow.js
+++ django/static/js/waterflow.js
@@ -50,11 +50,11 @@
             p.innerHTML = data[i].content;
             p.style.display = 'none';
             a.appendChild(p);
-            pic.onmouseover = function () {
+            info.onmouseover = function () {
                 p = this.getElementsByTagName('p');
                 p[0].style.display = 'block';
             }
-            pic.onmouseout = function () {
+            info.onmouseout = function () {
                 p = this.getElementsByTagName('p');
                 p[0].style.display = 'none';
             }
@@ -127,11 +127,11 @@
                     p.innerHTML = data[i].content;
                     p.style.display = 'none';
                     a.appendChild(p);
-                    pic.onmouseover = function () {
+                    info.onmouseover = function () {
                         p = this.getElementsByTagName('p');
                         p[0].style.display = 'block';
                     }
-                    pic.onmouseout = function () {
+                    info.onmouseout = function () {
                         p = this.getElementsByTagName('p');
                         p[0].style.display = 'none';
                     }
