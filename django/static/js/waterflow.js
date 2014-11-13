d2046626eda5d6fd5c8081465435abbce34a5266
= <loyaveforever@gmail.com>
2014-11-13 22:23:41 +0800

    change front page hover pattern

--- django/static/js/waterflow.js
+++ django/static/js/waterflow.js
@@ -50,11 +50,11 @@
             p.innerHTML = data[i].content;
             p.style.display = 'none';
             a.appendChild(p);
-            a.onmouseover = function () {
+            pic.onmouseover = function () {
                 p = this.getElementsByTagName('p');
                 p[0].style.display = 'block';
             }
-            a.onmouseout = function () {
+            pic.onmouseout = function () {
                 p = this.getElementsByTagName('p');
                 p[0].style.display = 'none';
             }
@@ -127,11 +127,11 @@
                     p.innerHTML = data[i].content;
                     p.style.display = 'none';
                     a.appendChild(p);
-                    a.onmouseover = function () {
+                    pic.onmouseover = function () {
                         p = this.getElementsByTagName('p');
                         p[0].style.display = 'block';
                     }
-                    a.onmouseout = function () {
+                    pic.onmouseout = function () {
                         p = this.getElementsByTagName('p');
                         p[0].style.display = 'none';
                     }
