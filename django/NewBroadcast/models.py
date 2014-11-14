a6da6ec5771663e7830b898e59cdc26b41c663ab
yuantailing <yuantailing@126.com>
2014-11-14 19:54:13 +0800

    数据库表项调整

--- django/NewBroadcast/models.py
+++ django/NewBroadcast/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 
 # Create your models here.
 
@@ -10,12 +10,15 @@ class User(models.Model):
     power = models.CharField(blank=False, default='user', max_length=32,
                              choices=(('user', 'user'), ('worker', 'worker'),
                                       ('admin', 'admin'),
-                                      ('superadmin', 'superadmin')))
+                                      ('superadmin', 'superadmin'),))
     birthday = models.DateTimeField(null=True, blank=True, default=None)
     phone_number = models.CharField(null=True, blank=True, default=None,
                                     max_length=32)
     update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
     create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + self.nickname + ' | ' + self.email
 
     def save(self):
         if not self.email:
@@ -37,33 +40,64 @@ class ProgramGroup(models.Model):
     update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + self.title
+    
     def save(self):
         if not self.title:
             self.title = None
         super(ProgramGroup, self).save()
 
 
+class ProgramSeries(models.Model):
+    title = models.TextField(blank=False, default=None)
+    order = models.IntegerField(blank=False, default=0)
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + self.title
+    
+    def save(self):
+        if not self.title:
+            self.title = None
+        super(ProgramSeries, self).save()
+
+
 class Program(models.Model):
     group = models.ForeignKey(ProgramGroup, related_name="program",
                               null=True, blank=True, default=None,
                               on_delete=models.SET_NULL)
-    series = models.TextField(null=True, blank=True, default=None)
+    series = models.ForeignKey(ProgramSeries, related_name="program",
+                              null=True, blank=True, default=None,
+                              on_delete=models.SET_NULL)
     title = models.TextField(blank=False, default=None)
     description = models.TextField(null=True, blank=True, default=None)
     weight = models.IntegerField(blank=False, default=0)
     click = models.IntegerField(blank=False, default=0)
+    praised = models.IntegerField(blank=False, default=0)
     recorder = models.TextField(null=True, blank=True, default=None)
     contributor = models.TextField(null=True, blank=True, default=None)
     workers = models.TextField(null=True, blank=True, default=None)
-    picture = models.TextField(null=True, blank=True, default=None) # json of list
-    audio = models.IntegerField(null=True, blank=True, default=True)
-    document = models.TextField(null=True, blank=True, default=None) # json of list
+    keyword = models.TextField(null=True, blank=True, default=None)
+    picture = models.TextField(null=True, blank=True, default='[]') # json of list
+    audio = models.IntegerField(null=True, blank=True, default=None)
+    document = models.TextField(null=True, blank=True, default='[]') # json of list
+    uploader = models.ForeignKey(User, related_name="program",
+                              null=True, blank=True, default=None,
+                              on_delete=models.SET_NULL)
     update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
-    def save(self):        
-        if not self.series:
-            self.series = None
+    def __unicode__(self):
+        res = '[' + str(self.id) + '] ' + self.title
+        if self.group:
+            res += ' | ' + self.group.title
+        if self.series:
+            res += ' | ' + self.series.title
+        return res
+    
+    def save(self):
         if not self.title:
             self.title = None
         if not self.description:
@@ -78,6 +112,15 @@ class Program(models.Model):
             self.picture = None
         if not self.document:
             self.document = None
+        self.keyword = self.title
+        if self.description:
+            self.keyword += '|' + self.description
+        if self.recorder:
+            self.keyword += '|' + self.recorder
+        if self.contributor:
+            self.keyword += '|' + self.contributor
+        if self.workers:
+            self.keyword += '|' + self.workers
         super(Program, self).save()
 
 
@@ -86,6 +129,9 @@ class Source(models.Model):
                            upload_to='source')
     md5 = models.TextField(null=True, blank=True, default=None)
 
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + os.path.split(self.document.file.name)[1]
+    
     def save(self):
         if not self.md5:
             self.md5 = None
@@ -103,7 +149,26 @@ class Comment(models.Model):
     update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + self.program.title + ' | ' + self.user.nickname
+    
     def save(self):
         if not self.content:
             self.content = None
         super(Comment, self).save()
+
+
+class Praise(models.Model):
+    user = models.ForeignKey(User, related_name="praise",
+                              null=False, blank=False, default=None,
+                              on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, related_name="praise",
+                              null=False, blank=False, default=None,
+                              on_delete=models.CASCADE)
+    # in Program filter: annotate(num_praise=Count('praise'))
+    update_time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '[' + str(self.id) + '] ' + self.program.title + ' | ' + self.user.nickname
+
