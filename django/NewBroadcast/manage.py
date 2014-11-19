675527b15f261eaef1ba9c92e51c3635c320add2
yuantailing <yuantailing@126.com>
2014-11-19 15:59:08 +0800

    NEWB-43 收藏夹渲染

--- django/NewBroadcast/manage.py
+++ django/NewBroadcast/manage.py
@@ -24,10 +24,12 @@ def power_trans(power_str):
 def show_space(req):
     user = User.objects.get(id=req.session['uid'])
     ups = Program.objects.filter(uploader=user).order_by('-create_time')[0:5]
+    favs = Program.objects.filter(favorite__in=user.favorite.all())
     return render_to_response("manage/space.html",
                               {'user':user,
                                'power':power_trans(user.power),
-                               'ups':ups, },
+                               'ups':ups,
+                               'favs':favs, },
                               context_instance=RequestContext(req));
 
 @power_required(['worker'])
